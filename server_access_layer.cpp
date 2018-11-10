889ac55c7883477ba76b33e7290221ec625b80a5
Bellkross <vasilenkowm@gmail.com>
2018-11-10 13:33:16 +0200

    Added server access layer with stubs

--- server_access_layer.cpp
+++ server_access_layer.cpp
@@ -12,7 +12,32 @@ ServerAccessLayer::~ServerAccessLayer()
 
 bool ServerAccessLayer::checkCredentials(const CardNumber& cardNumber, const Pin& pin)
 {
-	return true;/*_server.check(cardNumber, pin);*/
+	return true;// _server.check(cardNumber, pin);
+}
+
+Currency ServerAccessLayer::balance(const CardNumber& cn, const Pin& pin)
+{
+	return Currency(100,32); // _server.balance(cn, pin);
+}
+
+bool ServerAccessLayer::withdraw(const CardNumber& cn, const Pin& pin, const Currency& curr)
+{
+	return true; // _server.withdraw(cn,pin,curr);
+}
+
+bool ServerAccessLayer::deposit(const CardNumber& cn, const Pin& pin, const Currency& curr)
+{
+	return true; // _server.deposit(cn,pin,curr);
+}
+
+bool ServerAccessLayer::transact(const CardNumber& cn, const Currency& curr)
+{
+	return true; // _server.transact(cn,curr);
+}
+
+bool ServerAccessLayer::transact(const CardNumber& sender, const Pin& pin, const CardNumber& recipient, const Currency& curr)
+{
+	return true; // _server.transact(sender, pin, recipient, curr);
 }
 
 void encrypt(std::string& data)
--- server_access_layer.h
+++ server_access_layer.h
@@ -1,6 +1,7 @@
 #ifndef SERVER_ACCESS_LAYER_H_
 #define SERVER_ACCESS_LAYER_H_
 #include <string>
+#include "currency.h"
 #include "card_number.h"
 #include "pin.h"
 
@@ -8,10 +9,15 @@ class ServerAccessLayer
 {
 public:
 	//ServerAccessLayer(const Server&);
-	ServerAccessLayer();
+	ServerAccessLayer(); // make private after server appearing
 	~ServerAccessLayer();
 	bool checkCredentials(const CardNumber&, const Pin&);
-	
+	Currency balance(const CardNumber&, const Pin&);
+	bool withdraw(const CardNumber&, const Pin&, const Currency&);
+	bool deposit(const CardNumber&, const Pin&, const Currency&);
+	bool transact(const CardNumber&, const Currency&);
+	bool transact(const CardNumber&, const Pin&, const CardNumber&, const Currency&);
+
 private:
 	//const Server& _server;
 	ServerAccessLayer(const ServerAccessLayer&);
